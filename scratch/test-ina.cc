54cabd2a61f4de30ac5a068875acbfbe2109b05d
Paulo Alexandre Regis <regisin@gmail.com>
2018-04-10 15:15:51 -0700

    Update test-ina.cc

--- scratch/test-ina.cc
+++ scratch/test-ina.cc
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-///////////////aloha
+
 #include <iostream>
 #include "ns3/core-module.h"
 #include "ns3/ina219-module.h"
