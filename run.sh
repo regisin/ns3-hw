d2026f67825a9ad6470f82c33a5b14695d30bdc1
Paulo Alexandre Regis <regisin@gmail.com>
2018-04-11 09:47:57 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ echo "No simulation to run at start-up configured"
 echo "Edit me at: $(dirname $(readlink -f $0))/`basename $0`"
 
 # Uncomment/edit the line below to run the desired script:
-#./waf --run="test-ina --time=60 --charge=8000"
+#./waf --run="test-ina --time=10 --charge=8000" > test-ina.log 2>&1
