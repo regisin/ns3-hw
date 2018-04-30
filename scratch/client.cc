4ecf021b4fbbb651f8c71462c6b53eb00e6325fb
Paulo Alexandre Regis <regisin@gmail.com>
2018-04-30 15:01:09 -0700

    Update client.cc

--- scratch/client.cc
+++ scratch/client.cc
@@ -264,10 +264,10 @@ main (int argc, char *argv[])
   std::string dataRate("11Mb/s");
   
   std::string deviceName ("wlan0");
-  std::string ipAddr ("10.1.1.7");
+  std::string ipAddr ("10.1.1.XX");
   std::string dstAddr ("10.1.1.1");
   std::string netmask ("255.255.255.0");
-  std::string macAddr ("00:00:00:00:00:07");
+  std::string macAddr ("00:00:00:00:00:XX");
 
   Ipv4Address localIp = Ipv4Address (ipAddr.c_str ());
   Ipv4Address remoteIp = Ipv4Address (dstAddr.c_str ());
