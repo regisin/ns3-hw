5ed28b5e1736d2245f1a86abaf1da8707eb02a11
Paulo Alexandre Regis <regisin@gmail.com>
2018-05-14 13:41:54 -0700

    Create reconfigure.sh

---
+++ reconfigure.sh
@@ -0,0 +1,26 @@
+nodenumber=$(tail -c 3 /etc/hostname)
+
+sleep 10 # on avg. this is enough to have connection, otherwise will not resolve github.com address
+# update source-code
+git fetch --all
+git reset --hard origin/master
+
+# replace placeholders with correct node number for ip, and correct command to execute in run.sh
+sed -i -e "s/XX/$nodenumber/g" ./scratch/client.cc
+sed -i -e "s/XX/$nodenumber/g" ./scratch/relay.cc
+cmd=$(cat config.txt | grep $nodenumber: | cut -d ':' -f2)
+echo $cmd
+cmd=${cmd//\\/\\\\}
+echo $cmd
+cmd=${cmd//\//\\/}
+echo $cmd
+cmd=${cmd//./\\.}
+echo $cmd
+cmd=${cmd//&/\\&}
+echo $cmd
+sed -i -e "s/#command#/$cmd/g" ./run.sh
+
+./waf configure --enable-sudo
+./waf build
+
+cd
